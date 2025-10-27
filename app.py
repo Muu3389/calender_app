7f76779984ed0ecee807a898dbe316b2ebb796b5
Kosuk <muu2581@gmail.com>
2025-10-28 00:15:30 +0900

    add month

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
+import calendar
+from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/month")
+def month():
+    now = datetime.now()
+    cal = calendar.month(now.year, now.month)
+    return f"<pre>{cal}</pre>"
 
 if __name__ == "__main__":
     app.run(debug=True)
