60dbfa302ac5612a2d0846978eb25a8f17516ac5
muu2581 <muu2581@gmail.com>
2025-10-28 14:58:11 +0900

    style: カレンダーにマス目を追加

--- app.py
+++ app.py
@@ -5,21 +5,25 @@ from datetime import datetime
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     # クエリパラメータから年月を取得
     year = request.args.get("year", type=int)
     month = request.args.get("month", type=int)
 
+    # クエリパラメータがない場合は現在の年月を使用
     now = datetime.now()
     if not year or not month:
         year, month = now.year, now.month
 
-    cal = calendar.month(year, month)
+    # カレンダーのデータを生成
+    cal = calendar.Calendar(firstweekday=6)
+    month_days = cal.monthdayscalendar(year, month)
+
     return render_template(
         "index.html",
-        calendar_text=cal,
         year=year,
-        month=month
+        month=month,
+        month_days=month_days
     )
 
 if __name__ == "__main__":
